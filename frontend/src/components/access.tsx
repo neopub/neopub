7a9eae0a25da26ebedf4de809357ea5f0d17bf03
neopub <neopub@protonmail.com>
2021-10-21 07:14:23 -0700

    add title graphic

--- frontend/src/components/access.tsx
+++ frontend/src/components/access.tsx
@@ -38,10 +38,13 @@ export default function Access() {
 
   return (
     <div className="mt-24 flex flex-col items-center">
-      <h1 className="mb-4">neopub</h1>
+      <div className="flex flex-col items-center">
+        <img src="keyboard.png" className="w-40 pt-2 mb-2" />
+        <h1 className="mb-8">neopub</h1>
+      </div>
       <div className="flex flex-col">
         <p>Neopub is an <Link to="/arch">end-to-end encrypted</Link>, <a href="https://github.com/neopub/neopub">open-source</a>, <Link to="/host">self-hosted</Link> social network.</p>
-        <p>In neopub, you don't have an email and password. Your identity is a cryptographic keypair.</p>
+        <p className="mb-4">In neopub, you don't have an email and password. Your identity is a cryptographic keypair.</p>
         <button onClick={handleCreateIDClicked}>Create ID</button>
         <button onClick={handleLoadIDClicked}>Load ID</button>
         {status}
