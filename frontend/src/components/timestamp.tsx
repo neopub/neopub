f8ee2cbd9c7fc85818818be26ea0774f96654b18
neopub <neopub@protonmail.com>
2021-11-09 15:24:32 -0800

    full datetime for posts if not posted today

--- frontend/src/components/timestamp.tsx
+++ frontend/src/components/timestamp.tsx
@@ -1,5 +1,8 @@
 export default function Timestamp({ ts }: { ts: string }) {
   const date = new Date(ts);
+  const now = new Date();
 
-  return <div className="text-xs text-green-600">{date.toLocaleTimeString()}</div>;
+  const str = date.toLocaleDateString() !== now.toLocaleDateString() ? date.toLocaleString() : date.toLocaleTimeString();
+
+  return <div className="text-xs text-green-600">{str}</div>;
 }
