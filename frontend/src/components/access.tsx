27925c1900ea110b6ccd21053a6afb1bd0925633
neopub <neopub@protonmail.com>
2021-11-09 21:46:03 -0800

    de-emphasize load ID

--- frontend/src/components/access.tsx
+++ frontend/src/components/access.tsx
@@ -45,8 +45,8 @@ export default function Access() {
       <div className="flex flex-col">
         <p>Neopub is an <Link to="/arch">end-to-end encrypted</Link>, <a href="https://github.com/neopub/neopub">open-source</a>, <Link to="/host">self-hosted</Link> social network.</p>
         <p className="mb-4">In neopub, you don't have an email and password. Your identity is cryptographic.</p>
-        <button onClick={handleCreateIDClicked}>Create ID</button>
-        <button onClick={handleLoadIDClicked}>Load ID</button>
+        <button className="py-3" onClick={handleCreateIDClicked}>Create ID</button>
+        <button className="border-0" onClick={handleLoadIDClicked}>Load ID</button>
         {status}
       </div>
     </div>
