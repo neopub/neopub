f5137a7dc108287a55e29cf8af28a8399f301895
neopub <neopub@protonmail.com>
2021-11-10 20:21:00 -0800

    send anon replyer to make ID

--- frontend/src/components/postList.tsx
+++ frontend/src/components/postList.tsx
@@ -1,14 +1,19 @@
 import { IIndex, IEncPost } from "core/types";
 import { sendReply } from "lib/api";
+import { useHistory } from "react-router-dom";
 import EncryptedPost from "./encryptedPost";
 
 export default function PostList({ pubKeyHex, index, id, worldKeyHex, host }: { index: IIndex, id: string, worldKeyHex?: string, pubKeyHex?: string, host: string }) {
+  const history = useHistory();
+  
   if (index.posts?.length === 0) {
     return <div className="mt-2">No posts.</div>;
   }
 
   function handleReply(postId: string, pubKey: string) {
     if (!pubKeyHex) {
+      alert('You need an identity first.')
+      history.push(`/?next=${encodeURIComponent(history.location.pathname)}`);
       return;
     }
 
