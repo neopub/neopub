57cf0d905c580703e904db8d2bc46491b4c2fe27
neopub <neopub@protonmail.com>
2021-11-12 10:13:14 -0800

    refactor

--- frontend/src/components/timestamp.tsx
+++ frontend/src/components/timestamp.tsx
@@ -2,7 +2,8 @@ export default function Timestamp({ ts }: { ts: string }) {
   const date = new Date(ts);
   const now = new Date();
 
-  const str = date.toLocaleDateString() !== now.toLocaleDateString() ? date.toLocaleString() : date.toLocaleTimeString();
+  const isToday = date.toLocaleDateString() === now.toLocaleDateString();
+  const str = isToday ? date.toLocaleTimeString() : date.toLocaleString();
 
   return <div className="text-xs text-green-600">{str}</div>;
 }
