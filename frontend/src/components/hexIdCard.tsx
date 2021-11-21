7558e744533af509c68dbe1764692be8809ef0f4
neopub <neopub@protonmail.com>
2021-11-20 20:17:18 -0800

    handle missing handle

--- frontend/src/components/hexIdCard.tsx
+++ frontend/src/components/hexIdCard.tsx
@@ -2,6 +2,10 @@ import { hostPrefix } from "lib/net";
 import { Link } from "react-router-dom";
 import Hexatar from "./hexatar";
 
+function MissingHandle() {
+  return <i>No handle</i>;
+}
+
 export default function HexIDCard({ pubKey, host, handle }: { pubKey: string, host: string, handle?: string }) {
   let link = `/users/${pubKey}`;
   if (host !== hostPrefix) {
@@ -11,7 +15,7 @@ export default function HexIDCard({ pubKey, host, handle }: { pubKey: string, ho
   return (
     <Link className="flex flex-row items-center space-x-2 no-underline" to={link}>
       <Hexatar hex={pubKey} />
-      {handle && <div>{handle}</div>}
+      {<div>{handle ? handle : <MissingHandle />}</div>}
     </Link>
   )
 }
