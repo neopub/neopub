946093ee3a1bca01de339b85284a33ff5b6a4644
neopub <neopub@protonmail.com>
2021-11-21 22:32:43 -0800

    better timestamps

--- frontend/src/components/timestamp.tsx
+++ frontend/src/components/timestamp.tsx
@@ -3,7 +3,23 @@ export default function Timestamp({ ts }: { ts: string }) {
   const now = new Date();
 
   const isToday = date.toLocaleDateString() === now.toLocaleDateString();
-  const str = isToday ? date.toLocaleTimeString() : date.toLocaleString();
+  const isThisYear = date.getFullYear() === now.getFullYear();
+
+  const dateOpts = {
+    year: isThisYear ? undefined : "numeric",
+    month: "short",
+    day: "numeric",
+  } as const;
+
+  const timeOpts = {
+    hour: "numeric",
+    minute: "numeric",
+    second: undefined,
+  } as const;
+
+  const fmt = new Intl.DateTimeFormat([], isToday ? timeOpts : dateOpts)
+
+  const str = fmt.format(date);
 
   return <div className="text-xs text-green-600">{str}</div>;
 }
