ce5b68d2d67100f451dc8ee7028785fa0c3c81ae
neopub <neopub@protonmail.com>
2021-11-26 14:08:56 -0800

    style <Empty />

--- frontend/src/components/empty.tsx
+++ frontend/src/components/empty.tsx
@@ -1,8 +1,10 @@
 export default function Empty({ text, subtext }: { text: string, subtext?: string }) {
   return (
-    <div>
-      <h3 className="text-3xl">{text}</h3>
-      {subtext && <div className="max-w-md text-lg">{subtext}</div>}
+    <div className="flex justify-center my-8">
+      <div className="max-w-sm">
+        <h3 className="text-3xl font-bold">{text}</h3>
+        {subtext && <div className="max-w-md text-lg">{subtext}</div>}
+      </div>
     </div>
   );
 }
