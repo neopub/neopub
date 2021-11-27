1ffb79870edd7b752e3fa5636ff4b446b818bdd7
neopub <neopub@protonmail.com>
2021-11-27 14:05:02 -0800

    no period

--- frontend/src/components/subscriberList.tsx
+++ frontend/src/components/subscriberList.tsx
@@ -15,7 +15,7 @@ export default function SubscriberList() {
   }
 
   if (Object.values(subs).length < 1) {
-    return <Empty text="No followers." subtext="Share your QR code. When someone scans it and sends you a follow request, that will appear in your Inbox. When you accept the request, they'll appear here." />
+    return <Empty text="No followers" subtext="Share your QR code. When someone scans it and sends you a follow request, that will appear in your Inbox. When you accept the request, they'll appear here." />
   }
 
   return (
