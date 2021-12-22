b0214b410f038ed205d0da5c09c63c3d2bcd2f89
neopub <neopub@protonmail.com>
2021-12-21 18:13:46 -0800

    extract fetch implmenetation

--- cli/index.ts
+++ cli/index.ts
@@ -1,59 +1,17 @@
 import fs from "fs";
 import { bytes2hex, hex2bytes } from "./shared/core/bytes";
 import nodeCrypto from "crypto";
-import http from "http";
 import Net from "./shared/core/client/net";
 import Crypto from "./shared/core/crypto";
 import API from "./shared/core/client/api";
 import { ITextPost } from "./shared/core/types";
 import solvePoWChallenge from "./shared/core/challenge";
 import PoW, { numHashBits } from "./shared/core/pow";
+import { fetch } from "./lib";
+
+const hostPrefix = "";
 
 const crypto = nodeCrypto.webcrypto as any;
-
-// const hostPrefix = "http://localhost:8888";
-const hostPrefix = "";
-const host = "localhost";
-const port = "8888"
-
-async function fetch(path: string, init?: { method: "GET" | "POST" | "PUT" | "DELETE", headers?: Record<string, string>, body?: any }): Promise<any> {
-  return new Promise((resolve, reject) => {
-    const opts = {
-      hostname: host,
-      port,
-      path,
-      method: init?.method,
-      headers: init?.headers,
-    };
-
-    const req = http.request(opts, res => {
-      let resData: any;
-      res.on("data", d => {
-        resData = d;
-      });
-      res.on("close", () => {
-        resolve({
-          ok: res.statusCode === 200,
-          statusCode: res.statusCode,
-          statusMessage: res.statusMessage,
-          data: resData,
-          async arrayBuffer() {
-            return resData;
-          },
-          async text() {
-            return new TextDecoder().decode(resData);
-          },
-        });
-      })
-    });
-
-    if (init?.body) {
-      req.write(init?.body);
-    }
-
-    req.end();
-  });
-}
 
 const npCrypto = new Crypto(crypto);
 const net = new Net(hostPrefix, fetch, npCrypto, crypto);
